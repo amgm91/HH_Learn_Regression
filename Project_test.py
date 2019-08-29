58059a8fd3318dd11a435f2bb3c3376041b6967d
amgm91 <ahmedmghareeb91@gmail.com>
2019-08-29 18:08:44 +0200

    First commit

---
+++ Project_test.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from scipy.io import loadmat
+from scipy.stats import zscore
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+
+
+print("Hello Regression")
