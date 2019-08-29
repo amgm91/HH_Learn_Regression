2ba57fed24eaab790baf12c9222cf05a6accafc6
amgm91 <ahmedmghareeb91@gmail.com>
2019-08-29 18:52:25 +0200

    Data loaded Zscore scatter plot

--- Project_test.py
+++ Project_test.py
@@ -6,5 +6,41 @@ from scipy.stats import zscore
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 
+# -----------------------------------------------------------------------------
+# Load the data
+Data = loadmat('ChemTrainNew.mat')
 
-print("Hello Regression")
+#print(Data.keys())
+
+# .mat file keys
+# 'XtrainDS' --> Training Input data
+# 'YtrainDS' --> Training Output data
+# 'XtestDS' --> Testing Input data
+
+chem_in = pd.DataFrame(Data['XtrainDS'])
+chem_out = pd.DataFrame(Data['YtrainDS'])
+chem_test_in = pd.DataFrame(Data['XtestDS'])
+
+print()
+
+# chem_in -> 4466 samples * 65 variable --> Training Input data
+# chem_out -> 4466 samples * 1 variable --> Training Output data
+
+# -----------------------------------------------------------------------------------
+
+# Standrdize the input using z-score
+
+chem_in_std = chem_in.apply(zscore)
+
+# -----------------------------------------------------------------------------------
+
+# drawing scatter plots for each variable and output
+
+for i in range(len(chem_in.columns)):
+    plt.scatter(chem_in_std[i], chem_out, label="variable %d and output" %i)
+    plt.xlabel(("var %d" % i))
+    plt.ylabel("output")
+    plt.show()
+
+
+
